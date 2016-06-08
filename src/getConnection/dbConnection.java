a9829ba73ca6141236c8e263aa2ab4244a30c0d6
hengmengtang <heng.mengtang@gmail.com>
2016-06-08 07:06:44 +0700

    new update

---
+++ src/getConnection/dbConnection.java
@@ -0,0 +1,37 @@
+package getConnection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class dbConnection {
+	private static dbConnection instance;
+	private static Connection cnn=null;
+	
+	private dbConnection(){	
+	}
+	
+	public static dbConnection getInstance(){
+		if(instance==null){
+			instance=new dbConnection();
+		}
+		
+		return instance;
+	}
+	
+	public Connection getConnection(){
+		if(cnn==null){
+			try {
+				Class.forName("org.postgresql.Driver");
+				String url="jdbc:postgresql://localhost:5432/postgres";
+				String name="postgres";
+				String pass="1234";
+				cnn=DriverManager.getConnection(url,name,pass);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		
+		return cnn;
+	}
+	
+}
